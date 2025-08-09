3791fb6724290084d4c108ae3bde594a90752d0c
Peter Olsen <olse0321@umn.edu>
2025-08-09 09:47:55 -0500

    feat: add logger

--- go.mod
+++ go.mod
@@ -2,3 +2,5 @@
 module load-balancer
 
 go 1.24.6
+
+require github.com/google/uuid v1.6.0
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,36 @@
 package logger
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var logfile string = fmt.Sprintf("app_%s_%s.log",
+	time.Now().Format("2006-01-02"),
+	uuid.New().String()[:8],
+)
+
+func LogErr(msg string, err error) {
+	logLine := fmt.Sprintf("time=%s level=ERROR msg=\"%s\" error=\"%s\"", time.Now(), msg, err)
+	writeToFile(logLine)
+}
+
+func Log(msg string) {
+	logLine := fmt.Sprintf("time=%s level=INFO msg=\"%s\"", time.Now(), msg)
+	writeToFile(logLine)
+}
+
+func writeToFile(logLine string) {
+	f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Failed to open log file: %v\n", err)
+		return
+	}
+	defer f.Close()
+	if _, err := f.WriteString(logLine + "\n"); err != nil {
+		fmt.Printf("Failed to write to log file: %v\n", err)
+	}
+}
